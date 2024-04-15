e94b92a9b65039de73c36ac77886789f5fcee934
oopsss1 <142162317+oopsss1@users.noreply.github.com>
2024-04-15 12:58:59 +0800

    Update UserServiceImpl.java

--- payment_uniapp/payment_user/src/main/java/com/jhzf/service/impl/UserServiceImpl.java
+++ payment_uniapp/payment_user/src/main/java/com/jhzf/service/impl/UserServiceImpl.java
@@ -14,6 +14,44 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+    @Override
+    public ResponseDTO regist(LoginVo loginVo) {
+        int res = userMapper.selectUser(loginVo.getAccount());
+        if(res == 1){
+            return ResponseDTO.error(0,"注册的手机号已存在");
+        }else{
+            System.out.println(loginVo.getAccount() + " " + loginVo.getPwd() + " " + loginVo.getNickName());
+            userMapper.regist(loginVo.getAccount(), Md5.getString(loginVo.getPwd()),loginVo.getNickName());
+            return ResponseDTO.success(200,"注册成功");
+        }
+    }
+
+    @Override
+    public ResponseDTO accountLogin(String account, String pwd) {
+        int res = userMapper.accountLogin(account,Md5.getString(pwd));
+        if(res == 1){
+            return ResponseDTO.success(200,"登录成功");
+        }else{
+            return ResponseDTO.error(0,"登录失败，账号或密码错误");
+        }
+    }
+
+    @Override
+    public ResponseDTO messageCodeLogin(String account, String code) {
+        int count = userMapper.selectUser(account);
+        if(count == 1){
+            String messageCode = redisTemplate.opsForValue().get(account);
+            if(code.equals(messageCode)){
+                return ResponseDTO.success(200,"登录成功");
+            }else{
+                return ResponseDTO.error(0,"验证码错误");
+            }
+        }else{
+            return ResponseDTO.error(0,"账号不存在,请先去注册");
+        }
+    }
 
     @Override
     public ResponseDTO modifyPwd(ModifyPwdVo vo) {
