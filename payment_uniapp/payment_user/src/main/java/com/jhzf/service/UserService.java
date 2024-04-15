a6a3c932260e721e78fbc9328b4b87c391711b8f
oopsss1 <142162317+oopsss1@users.noreply.github.com>
2024-04-15 12:19:44 +0800

    Update UserService.java

--- payment_uniapp/payment_user/src/main/java/com/jhzf/service/UserService.java
+++ payment_uniapp/payment_user/src/main/java/com/jhzf/service/UserService.java
@@ -5,4 +5,12 @@ import com.jhzf.vo.user.ModifyPwdVo;
 
 public interface UserService {
     ResponseDTO modifyPwd(ModifyPwdVo vo);
+    
+    ResponseDTO regist(LoginVo loginVo);
+
+    // 账号密码登录
+    ResponseDTO accountLogin(String account, String pwd);
+
+    // 短信验证码登录
+    ResponseDTO messageCodeLogin(String account, String code);
 }
