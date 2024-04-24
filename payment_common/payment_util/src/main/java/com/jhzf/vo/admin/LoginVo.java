3269bf3b41ac0afa5f6bd0960ff4515e8e5d9476
linxiaolong595 <979920090@qq.com>
2024-04-24 10:11:34 +0800

    ...

---
+++ payment_common/payment_util/src/main/java/com/jhzf/vo/admin/LoginVo.java
@@ -0,0 +1,15 @@
+package com.jhzf.vo.admin;
+
+import lombok.Data;
+
+/**
+ * Description:
+ *
+ * @Author： 林晓龙
+ * @DATE: 2024/4/23 16:11
+ */
+@Data
+public class LoginVo {
+    private String username;
+    private String password;
+}
