171cfe027dce68192934647692f45772601309ad
A-Dogs <972543376@qq.com>
2024-04-29 14:59:59 +0800

    更新了提现审核接口和审核详情接口

--- payment_back/payment_main/src/main/java/com/jhzf/service/WithdrawalService.java
+++ payment_back/payment_main/src/main/java/com/jhzf/service/WithdrawalService.java
@@ -1,8 +1,13 @@
 package com.jhzf.service;
 
+import com.jhzf.util.ResponseDTO;
+import com.jhzf.vo.order.WithdrawalVo;
+
 /**
  * @author 吴政顺
  * @date 2024/4/26 17:10
  */
 public interface WithdrawalService {
+    ResponseDTO WithdrawalOlder(WithdrawalVo vo);//查询提现审核订单
+    ResponseDTO WithdrawalDetails(int payoutsId);//提现订单详情
 }
